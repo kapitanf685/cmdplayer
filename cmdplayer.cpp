920b49a88a1fa8fbe8b42ea46d83a40516f05ce8
kapitanf685 <111685397+kapitanf685@users.noreply.github.com>
2022-08-25 14:11:26 +0200

    First Addition

---
+++ cmdplayer.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+#include <dshow.h>
+#include <strmif.h>
+#include <control.h>
+#include <winuser.h>
+
+#pragma comment(lib, "ole32")
+#pragma comment(lib, "strmiids")
+#pragma comment(lib, "user32")
+
+using namespace std;
+
+wchar_t *convertCharArrayToLPCWSTR(const char* charArray)
+{
+    wchar_t* wString=new wchar_t[4096];
+    MultiByteToWideChar(CP_ACP, 0, charArray, -1, wString, 4096);
+    return wString;
+}
+
+int main(int argc, char** argv)
+{
+    cout << "cmdplayer v0.2" << endl;
+    
+    int hr = CoInitialize(NULL);
+    if (FAILED(hr)) 
+    {
+        cout << "Initialization of COM Object failed." << endl;
+        goto endprogram;
+    }
+
+    IGraphBuilder *gbuilder;
+    hr = CoCreateInstance(CLSID_FilterGraph, NULL, CLSCTX_INPROC_SERVER, IID_IFilterGraph, (void**)&gbuilder);
+    if (FAILED(hr)) 
+    {
+        cout << "CreateInstance of GraphBuilder failed." << endl;
+        goto endprogram;
+    }    
+
+    IMediaControl *control;
+    IMediaEvent *event;
+    hr = gbuilder->QueryInterface(IID_IMediaControl, (void**)&control);    
+    if (FAILED(hr)) 
+    {
+        cout << "MediaControl of GraphBuilder failed." << endl;
+        goto endprogram;
+    }
+    hr = gbuilder->QueryInterface(IID_IMediaEvent, (void**)&event);
+    if (FAILED(hr))
+    {
+        cout << "MediaEvent of GraphBuilder failed." << endl;
+        goto endprogram;
+    }
+
+   IBaseFilter* filter0;
+   hr = CoCreateInstance(CLSID_VideoMixingRenderer, NULL, CLSCTX_INPROC_SERVER, IID_IBaseFilter, (void**)&filter0);
+   if (FAILED(hr))
+   {
+       cout << "Create filter0 failed." << endl;
+       goto endprogram;
+   }
+   hr = gbuilder->AddFilter(filter0, L"Decoder");
+   if (FAILED(hr))
+   {
+       cout << "Addition filter0 to GraphBuilder failed." << endl;
+       goto endprogram;
+   }
+
+   IVideoWindow* window0;
+   hr = filter0->QueryInterface(IID_IVideoWindow, (void**)&window0);
+   if (FAILED(hr))
+    {
+        cout << "VideoWindow of GraphBuilder failed." << endl;
+        goto endprogram;
+    }  
+
+    //***************
+    //CLSID_FileSource
+    //***************
+    if (argc<=1)
+    {
+        goto endprogram;
+    }    
+    LPCWSTR filename = convertCharArrayToLPCWSTR(argv[1]);
+    hr = gbuilder->RenderFile(filename,NULL);
+    if (FAILED(hr)) 
+    {
+        cout << "RenderFile failed." << endl;
+        goto endprogram;
+    }    
+
+    window0->put_WindowStyle(WS_EX_COMPOSITED);
+
+    if (SUCCEEDED(hr))
+    { 
+        cout << "Press CTRL+C to exit playing..." << endl;
+        hr = control->Run();
+        if (SUCCEEDED(hr))
+        {
+                long EvCode;
+                event->WaitForCompletion(-1, &EvCode);
+        }      
+    }
+
+    control->Stop();
+
+    endprogram:
+        control->Release();
+        event->Release();
+        gbuilder->Release();    
+}
---
+++ compile.bat
@@ -0,0 +1,22 @@
+@echo off
+set PATH=%PATH%;c:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Tools\MSVC\14.33.31629\bin\Hostx64\x64\
+
+rem set PATH=%PATH%;c:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\
+rem vcvars64.bat
+
+set INCLUDE=c:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Tools\MSVC\14.33.31629\include
+set INCLUDE=%INCLUDE%;c:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt
+set INCLUDE=%INCLUDE%;c:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\um
+set INCLUDE=%INCLUDE%;c:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\shared\
+
+set LIB=c:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Tools\MSVC\14.33.31629\lib\x64\
+set LIB=%LIB%;c:\Program Files (x86)\Windows Kits\10\Lib\10.0.19041.0\um\x64\
+set LIB=%LIB%;c:\Program Files (x86)\Windows Kits\10\Lib\10.0.19041.0\ucrt\x64\
+
+<<<<<<< HEAD
+@cl.exe /EHsc vpxdecoderidl.c cmdplayer.cpp /ocmdplay.exe
+=======
+@cl.exe /EHsc cmdplayer.cpp /ocmdplay.exe
+>>>>>>> 9a81c59 (Rebuild Souces)
+
+pause
